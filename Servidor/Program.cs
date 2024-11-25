d44e74415b1f5354e6d3e31ec72acbc8d7f2a4db
m1ca33lo <dmicaellozoppi@outlook.com>
2024-11-26 00:13:15 +0100

    Add project files.

---
+++ Servidor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Servidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Servidor_Chat Chat = new Servidor_Chat();
+            }
+        }
+    }
+}
